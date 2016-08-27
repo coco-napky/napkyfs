b80399c710e581d10779a9775383a7b33bc1402e
coco-napky <coco.napky@gmail.com>
2016-08-27 00:10:39 -0700

    Sync.js
    Synchronous API wrapper  around FileSystem Asynchronous operations

---
+++ sync.js
@@ -0,0 +1,46 @@
+"use strict";
+
+let fs = {};
+
+const init =  filesystem => fs = filesystem
+
+async function mount(unit) {
+	try { await fs.mountUnit(unit);
+	} catch(e){
+		console.log(e.message)
+	}
+}
+
+async function create(unit, blockSize, blocks) {
+	try {
+		await fs.createUnit(unit, blockSize, blocks);
+	} catch(e){
+		console.log(e.message)
+	}
+}
+
+async function importFile(path, dst) {
+	try {
+		await fs.importFile(path, dst);
+	} catch(e){
+		console.log(e.message)
+	}
+}
+
+async function exportFile(file, dst) {
+	try {
+		await fs.exportFile(file, dst);
+	} catch(e){
+		console.log(e.message)
+	}
+}
+
+async function deleteFile(file) {
+	try {
+		await fs.deleteFile(file);
+	} catch(e){
+		console.log(e.message)
+	}
+}
+
+export default { init, mount, create, importFile, exportFile, deleteFile }
